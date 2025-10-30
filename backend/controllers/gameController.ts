3d278266283432b0e0003ede0dcf761f1ce4e11c
twangyal <tsering.w1029@gmail.com>
2025-10-30 15:48:00 -0700

    controller template

---
+++ backend/controllers/gameController.ts
@@ -0,0 +1,19 @@
+export const generateMainProblem = async (req: Request, res: Response) => {
+    // logic
+};
+export const generateMatchingProblem = async (req: Request, res: Response) => {
+    // logic
+};
+export const generateMazeProblem = async (req: Request, res: Response) => {
+    // logic
+};
+
+export const handleMainProblem = async (req: Request, res: Response) => {
+    // logic
+};
+export const handleMatchingProblem = async (req: Request, res: Response) => {
+    // logic
+};
+export const handleMazeProblem = async (req: Request, res: Response) => {
+    // logic
+};
