a173e02a3ea8a7c23da187807182977e155b366e
Ralph Inaki Valdez <ralphinakivaldez@Ralphs-MacBook-Pro.local>
2025-10-30 14:51:25 -0700

    Implemented the structure of the GraphQuestion class

--- frontend/src/screens/graph-game/GraphScreenModel.ts
+++ frontend/src/screens/graph-game/GraphScreenModel.ts
@@ -1,6 +1,31 @@
+import type { Question } from "../../types";
+
 /**
  * Model for the Graphing Game Module
  */
 export class GraphScreenModel {
+    private question: GraphQuestion;
 
+    constructor() {
+        this.question = new GraphQuestion();
+    }
+}
+
+/**
+ * Generate and store answer values for the Graphing Game
+ */
+class GraphQuestion implements Question {
+    private questionType: string;
+
+    constructor() {
+        this.questionType = "";
+    }
+
+    generateAnswerValues(): void {
+
+    }
+
+    verifyAnswer(): boolean {
+        return true;
+    }
 }
