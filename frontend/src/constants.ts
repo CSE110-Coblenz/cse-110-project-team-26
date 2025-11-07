391d13b5cee90ae1c3b9feccd5b83cb1730919f6
Ralph Inaki Valdez <ralphinakivaldez@Ralphs-MacBook-Pro.local>
2025-11-07 10:26:15 -0800

    Added function to generate random numbers and created string constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -4,3 +4,11 @@ export const STAGE_HEIGHT = 600;
 
 // Game settings
 export const GAME_DURATION = 10; // seconds
+
+export const LINEAR = "LINEAR";
+export const QUADRATIC = "QUADRATIC";
+export const ABSVAL = "ABSVAL";
+
+export function generateRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
