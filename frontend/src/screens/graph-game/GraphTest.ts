cdb0c1da1cc9d8b30b37a899a14bc7e52986f8b1
Vince Montegrande <vlmontegrande@gmail.com>
2025-11-09 13:02:14 -0800

    Add localhost testing functionality

---
+++ frontend/src/screens/graph-game/GraphTest.ts
@@ -0,0 +1,32 @@
+import Konva from "konva";
+import type { ScreenSwitcher, Screen } from "../../types.ts";
+import { GraphScreenController } from "./GraphScreenController.ts";
+import { STAGE_WIDTH, STAGE_HEIGHT } from  "../../constants.ts";
+
+class App implements ScreenSwitcher {
+  private stage: Konva.Stage;
+  private layer: Konva.Layer;
+
+  private graphScreenController: GraphScreenController;
+  
+  constructor(container: string) {
+    this.stage = new Konva.Stage({
+      container,
+      width: STAGE_WIDTH,
+      height: STAGE_HEIGHT,
+    });
+
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer);
+
+    this.graphScreenController = new GraphScreenController(this);
+
+    this.layer.add(this.graphScreenController.getView().getGroup());
+
+    this.layer.draw();
+
+    this.graphScreenController.getView().show();
+  }
+}
+
+new App("container");
