37e648ce02244e33011800d1ab81b578017ad67f
Vince Montegrande <vlmontegrande@gmail.com>
2025-11-10 19:41:40 -0800

    Refactor code to put elements in nested groups

--- frontend/src/screens/graph-game/GraphScreenView.ts
+++ frontend/src/screens/graph-game/GraphScreenView.ts
@@ -5,7 +5,10 @@ import { STAGE_WIDTH, STAGE_HEIGHT } from "../../constants";
 // SCREEN SPECIFIC CONSTANTS
 
 const OFFSET = STAGE_WIDTH * 0.02;
+const PANEL_WIDTH = STAGE_WIDTH * (1/5);
 const BOX_WIDTH = STAGE_WIDTH * (1/5) - OFFSET;
+
+// Background, stays in the root group
 
 const BACKGROUND_PROPERTIES = {
   x: 0,
@@ -15,37 +18,56 @@ const BACKGROUND_PROPERTIES = {
   fill: "#8B8B8B"
 };
 
+// Graph group and elements
+
+const GRAPH_GROUP_PROPERTIES = {
+  x: PANEL_WIDTH,
+  y: 0,
+  width: STAGE_WIDTH - PANEL_WIDTH,
+  height: STAGE_HEIGHT
+};
+
+const GRAPH_BACKGROUND_PROPERTIES = {
+  x: OFFSET,
+  y: OFFSET,
+  width: GRAPH_GROUP_PROPERTIES.width - OFFSET,
+  height: GRAPH_GROUP_PROPERTIES.height - OFFSET * 2,
+  fill: "#161313"
+};
+
+// Sprite group and elements
+
+const SPRITE_GROUP_PROPERTIES = {
+  x: 0,
+  y: 0,
+  width: PANEL_WIDTH,
+  height: STAGE_HEIGHT * (2/7)
+};
+
 const SPRITE_BOX_PROPERTIES = {
   x: OFFSET,
   y: OFFSET,
   width: BOX_WIDTH,
-  height: STAGE_HEIGHT * (2/7) - OFFSET,
+  height: SPRITE_GROUP_PROPERTIES.height - (OFFSET * 2),
   fill: "#FFF3F3"
+};
+
+// Dialogue group and elements
+
+const DIALOGUE_GROUP_PROPERTIES = {
+  x: 0,
+  y: SPRITE_GROUP_PROPERTIES.height,
+  width: PANEL_WIDTH,
+  height: STAGE_HEIGHT * (3/7)
 };
 
 const DIALOGUE_BOX_PROPERTIES = {
   x: OFFSET,
-  y: SPRITE_BOX_PROPERTIES.height + (2 * OFFSET),
+  y: 0,
   width: BOX_WIDTH,
-  height: STAGE_HEIGHT * (3/7) - OFFSET,
+  height: DIALOGUE_GROUP_PROPERTIES.height - OFFSET,
   fill: "#413434"
 };
-
-const INPUT_AND_EQUATION_BOX_PROPERTIES = {
-  x: OFFSET,
-  y: SPRITE_BOX_PROPERTIES.height + DIALOGUE_BOX_PROPERTIES.height + (3 * OFFSET),
-  width: BOX_WIDTH,
-  height: STAGE_HEIGHT * (2/7) - (OFFSET * 2),
-  fill: "#D9D9D9"
-}
-
-const EQUATION_BOX_PROPERTIES = {
-  x: OFFSET * (1.5),
-  y: INPUT_AND_EQUATION_BOX_PROPERTIES.y + OFFSET * (0.5),
-  width: BOX_WIDTH - (OFFSET),
-  height: INPUT_AND_EQUATION_BOX_PROPERTIES.height * (1/4) - OFFSET,
-  fill: "#110808"
-}
 
 const DIALOGUE_TEXT_PROPERTIES = {
   x: DIALOGUE_BOX_PROPERTIES.x,
@@ -56,7 +78,32 @@ const DIALOGUE_TEXT_PROPERTIES = {
   fontSize: 24,
   fontFamily: "Arial",
   fill: "white"
-}
+};
+
+// Equation/Input group and elements
+
+const INPUT_AND_EQUATION_GROUP_PROPERTIES = {
+  x: 0,
+  y: DIALOGUE_GROUP_PROPERTIES.height + SPRITE_GROUP_PROPERTIES.height,
+  width: PANEL_WIDTH,
+  height: STAGE_HEIGHT * (2/7)
+};
+
+const INPUT_AND_EQUATION_BOX_PROPERTIES = {
+  x: OFFSET,
+  y: 0,
+  width: BOX_WIDTH,
+  height: INPUT_AND_EQUATION_GROUP_PROPERTIES.height - OFFSET,
+  fill: "#D9D9D9"
+};
+
+const EQUATION_BOX_PROPERTIES = {
+  x: INPUT_AND_EQUATION_BOX_PROPERTIES.x + (OFFSET * (0.5)),
+  y: INPUT_AND_EQUATION_BOX_PROPERTIES.y + (OFFSET * (0.5)),
+  width: BOX_WIDTH - (OFFSET),
+  height: (INPUT_AND_EQUATION_BOX_PROPERTIES.height * (1/4)) - OFFSET,
+  fill: "#110808"
+};
 
 const EQUATION_TEXT_PROPERTIES = {
   x: EQUATION_BOX_PROPERTIES.x,
@@ -68,15 +115,8 @@ const EQUATION_TEXT_PROPERTIES = {
   fontFamily: "Arial",
   fill: "white",
   align: "center"
-}
+};
 
-const GRAPH_BACKGROUND_PROPERTIES = {
-  x: BOX_WIDTH + OFFSET * 2,
-  y: OFFSET,
-  width: STAGE_WIDTH - BOX_WIDTH - OFFSET * 3,
-  height: STAGE_HEIGHT - OFFSET * 2,
-  fill: "#161313"
-}
 
 /**
  * View for the Graphing game module
@@ -93,36 +133,20 @@ export class GraphScreenView implements View {
     constructor(onEquationSubmission?: () => boolean) {
         this.group = new Konva.Group({ visible: false });
 
+        // Root group elements
+        
         const background = new Konva.Rect({
             ...BACKGROUND_PROPERTIES
         });
 
+        // Sprite group elements
+
+        const spriteGroup = new Konva.Group({
+            ...SPRITE_GROUP_PROPERTIES
+        });
+
         const spriteBox = new Konva.Rect({
             ...SPRITE_BOX_PROPERTIES
-        });
-
-        const dialogueBox = new Konva.Rect({
-            ...DIALOGUE_BOX_PROPERTIES
-        });
-
-        const inputAndEquationBox = new Konva.Rect({
-            ...INPUT_AND_EQUATION_BOX_PROPERTIES
-        });
-
-        const equationBox = new Konva.Rect({
-            ...EQUATION_BOX_PROPERTIES
-        });
-
-        const graphBackground = new Konva.Rect({
-            ...GRAPH_BACKGROUND_PROPERTIES
-        });
-
-        this.dialogueText = new Konva.Text({
-            ...DIALOGUE_TEXT_PROPERTIES
-        });
-
-        this.equationText = new Konva.Text({
-            ...EQUATION_TEXT_PROPERTIES
         });
         
         // This is a placeholder for the sprite
@@ -139,7 +163,60 @@ export class GraphScreenView implements View {
             verticalAlign: "middle"
         });
 
-        this.group.add(background,
+        spriteGroup.add(spriteBox, spriteText);
+
+        // Dialogue group elements
+
+        const dialogueGroup = new Konva.Group({
+            ...DIALOGUE_GROUP_PROPERTIES
+        });
+
+        const dialogueBox = new Konva.Rect({
+            ...DIALOGUE_BOX_PROPERTIES
+        });
+
+        this.dialogueText = new Konva.Text({
+            ...DIALOGUE_TEXT_PROPERTIES
+        });
+
+        dialogueGroup.add(dialogueBox, this.dialogueText);
+
+        // Input/Equation group elements
+
+        const inputAndEquationGroup = new Konva.Group({
+            ...INPUT_AND_EQUATION_GROUP_PROPERTIES
+        });
+
+        const inputAndEquationBox = new Konva.Rect({
+            ...INPUT_AND_EQUATION_BOX_PROPERTIES
+        });
+
+        const equationBox = new Konva.Rect({
+            ...EQUATION_BOX_PROPERTIES
+        });
+
+        this.equationText = new Konva.Text({
+            ...EQUATION_TEXT_PROPERTIES
+        });
+
+        inputAndEquationGroup.add(inputAndEquationBox, equationBox, this.equationText);
+
+        // Graph group elements
+
+        const graphGroup = new Konva.Group({
+            ...GRAPH_GROUP_PROPERTIES
+        });
+
+        const graphBackground = new Konva.Rect({
+            ...GRAPH_BACKGROUND_PROPERTIES
+        });
+
+        graphGroup.add(graphBackground);
+
+        this.group.add(background, spriteGroup, dialogueGroup, inputAndEquationGroup, graphGroup);
+
+        
+        /*this.group.add(background,
             spriteBox,
             dialogueBox,
             inputAndEquationBox,
@@ -148,7 +225,8 @@ export class GraphScreenView implements View {
             this.dialogueText,
             this.equationText,
             spriteText // Sprite placeholder
-        );
+        );*/
+
     }
 
     /**
