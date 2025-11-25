a9271de10bf5375779a93d98daa49ee32fd5bc5a
Vince Montegrande <vlmontegrande@gmail.com>
2025-11-25 10:17:50 -0800

    Add model test files

---
+++ frontend/src/screens/graph-game/tests/model.test.ts
@@ -0,0 +1,70 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { GraphScreenModel } from "./GraphScreenModel";
+import { LINEAR, ABSVAL, QUADRATIC } from "../../constants";
+import { Linear, AbsoluteValue, Quadratic } from "../../types";
+import type { EquationAnswerFormat } from "../../types";
+
+describe("model", () => {
+    describe("constructor", () => {
+        it("should handle linear questions", () => {
+            const model = new GraphScreenModel(0);
+            expect(model.getQuestionType()).toBe(LINEAR);
+        });
+        it("should handle quadratic questions", () => {
+            const model = new GraphScreenModel(1);
+            expect(model.getQuestionType()).toBe(QUADRATIC);
+        });
+        it("should handle absolute value questions", () => {
+            const model = new GraphScreenModel(2);
+            expect(model.getQuestionType()).toBe(ABSVAL);
+        });
+    });
+
+    describe("reset", () => {
+        const model = new GraphScreenModel(0);
+        for (let i = 0; i < 10; i++) {
+            const initialAnswer = model.getAnswer();
+            model.reset();
+            const type = model.getQuestionType();
+            expect([LINEAR, QUADRATIC, ABSVAL]).toContain(type);
+            expect(model.getAnswer()).toBeDefined();
+            expect(model.getAnswer()).not.toBe(initialAnswer);
+        }
+    });
+
+    describe("verifyAnswer", () => {
+        const model = new GraphScreenModel(0);
+        const answer = model.getAnswer();
+        expect(model.verifyAnswer(answer)).toBe(true);
+
+        const incorrectLinear = new Linear(false);
+        expect(model.verifyAnswer(incorrectLinear)).toBe(false);
+
+        const incorrectQuadratic = new Quadratic(false);
+        expect(model.verifyAnswer(incorrectAbsoluteValue)).toBe(false);
+
+        const incorrectAbsoluteValue = new AbsoluteValue(false);
+        expect(model.verifyAnswer(incorrectAbsoluteValue)).toBe(false);
+    });
+
+    describe("dialogue", () => {
+        const model = new GraphScreenModel(0);
+        model.setDialogue("Hello World!");
+        expect(model.getDialogue()).toEqual("Hello World!");
+
+        model.setDialogue("");
+        expect(model.getDialogue()).toEqual("");
+    });
+
+    describe("sprite", () => {
+        const model = new GraphScreenModel(0);
+        const mockImage = new Image();
+        const newMockImage = new Image();
+
+        model.setSprite(mockImage);
+        expect(model.getSprite()).toEqual(mockImage);
+
+        model.setSprite(newMockImage);
+        expect(model.getSprite()).toEqual(newMockImage);
+    })
+});
